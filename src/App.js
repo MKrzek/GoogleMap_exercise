34c49b7ffdb4191e991d5c51a045e5faa55ee4cc
Malgorzata <mkrzek@googlemail.com>
2018-02-05 16:24:10 +0000

    second

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+  getInitialState(){
+    let favourites=[];
+    if (localStorage.favourites){
+      favourites=JSON.parse(localStorage.favourites)
+    }
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         
       </div>
     );
